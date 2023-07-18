72ba90d5f45e71ea610fd4312081b1acdff56a97
Dmitry Leonov <leonov.do72@gmail.com>
2023-07-18 23:48:35 +0300

    Commit lesson 3

--- lesson3/mylist2.py
+++ lesson3/mylist2.py
@@ -9,7 +9,22 @@
 
 
 class MyList2:
-    pass
+    def __init__(self, data):
+        self.data = data
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.data):
+            raise StopIteration
+        value = self.data[self.index]
+        self.index += 1
+        return value
+
+    def __getitem__(self, index):
+        return self.data[index]
 
 
 my_list = MyList2([1, 2, 3])
--- lesson3/timer.py
+++ lesson3/timer.py
@@ -5,13 +5,20 @@
 - __exit__(self, exc_type, exc_val, exc_tb): магический метод, который останавливает таймер
 и выводит время выполнения блока кода.
 """
+import time
 
 
 class Timer:
-    pass
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.end_time = time.time()
+        self.elapsed_time = self.end_time - self.start_time
 
 
 with Timer() as timer:
-    # блок кода
+    time.sleep(1)
 
 print("Execution time:", timer.elapsed_time)
